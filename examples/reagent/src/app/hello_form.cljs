e240142825120d22f3b5e6cb6e9a1ffb2b289b49
Jacob Maine <jacob.maine@gmail.com>
2021-06-18 03:01:34 -0700

    Show off active tracking in reagent example

--- examples/reagent/src/app/hello_form.cljs
+++ examples/reagent/src/app/hello_form.cljs
@@ -2,6 +2,7 @@
   (:require [f-form.dom :as form.dom]
             [f-form.field :as field]
             [f-form.form :as form]
+            [f-form.field-tracker :as form.field-tracker]
             [f-form.validation :as form.validation]
             [f-form.validation.vlad :as form.vlad]
             [reagent.core :as r]
@@ -16,17 +17,25 @@
        ^{:key i}
        [:li error])]))
 
-(defn errors
-  [{:keys [field/touched? field/errors field/warnings]}]
-  (when (or (seq errors) (seq warnings))
-    [:div.space-y-1
-     [errors-list touched? :text-yellow warnings]
-     [errors-list touched? :text-red errors]]))
+(defn field-status [{:keys [field/active? field/touched? field/errors field/warnings]}]
+  [:div.flex.space-x-1
+   [:span.w-4 {:class (cond
+                        (seq errors)   :text-red
+                        (seq warnings) :text-yellow
+                        :else          :text-green)}
+    (cond
+      active?                          "←"
+      (not touched?)                   ""
+      (or (seq errors) (seq warnings)) "x"
+      :else                            "✓")]
+   [:div.space-y-1
+    [errors-list touched? :text-yellow warnings]
+    [errors-list touched? :text-red errors]]])
 
 (def field-labels
   {[:full-name]             "Full Name"
    [:security-question]     "Security Question"
-   [:security-response]     "Response"
+   [:security-response]     "Security Question Response"
    [:password]              "Password"
    [:password-confirmation] "Password Confirmation"
    [:premium-account]       "Premium Account"})
@@ -61,14 +70,16 @@
 (defn validate-form [form]
   (form.vlad/validate form (validation form) field-labels))
 
+(def tracker (form.field-tracker/tracker #{:field/active? :field/touched?}))
+
 (def hello-form
   (r/atom (validate-form
-           (form/init [(field/init [:full-name] "John Doe")
-                       (field/init [:security-question])
-                       (field/init [:security-response])
-                       (field/init [:password])
-                       (field/init [:password-confirmation])
-                       (field/init [:premium-account] false)]))))
+           (form/init [(field/init [:full-name] "John Doe" tracker)
+                       (field/init [:security-question] nil tracker)
+                       (field/init [:security-response] nil tracker)
+                       (field/init [:password] nil tracker)
+                       (field/init [:password-confirmation] nil tracker)
+                       (field/init [:premium-account] false tracker)]))))
 
 (defn update-field [path update-fn _dom-event]
   (swap! hello-form (fn [form]
@@ -77,49 +88,42 @@
 (defn basic-input [props field]
   [:<>
    [:label {:for (:id props)} [:span (field-label field)]]
-   [:div.flex.flex-column.space-y-1
-    [:input
+   [:div
+    [:input.w-full
      (form.dom/input (assoc props :on-change update-field)
-                     field)]
-    [errors field]]])
+                     field)]]
+   [field-status field]])
 
 (defn password-input [props field]
   [basic-input (assoc props :type "password") field])
 
-(defn security-question-input [question-field response-field]
+(defn security-question-input [field]
   [:<>
-   [:label {:for "field-question"} [:span (field-label question-field)]]
-   [:div]
-   [:div.flex.flex-column.space-y-1
+   [:label {:for "field-question"} [:span (field-label field)]]
+   [:div
     [:select#field-question
      (f-form.dom/select {:on-change update-field}
-                        question-field
+                        field
                         {:options      security-questions
                          :option-value :question/id})
      [:option {:value "" :disabled true} "Choose..."]
      (for [{:keys [question/id question/text]} security-questions]
        ^{:key id}
-       [:option {:value id} text])]
-    [errors question-field]]
-   [:div.flex.flex-column.space-y-1
-    [:label [:span.sr-only (field-label response-field)]
-     [:input
-      (form.dom/input {:on-change update-field}
-                      response-field)]]
-    [errors response-field]]])
+       [:option {:value id} text])]]
+   [field-status field]])
 
 (defn premium-account-input [field]
   [:<>
    [:label {:for "field-premium-account-acceptance"} [:span (field-label field)]]
-   [:div.space-y-1
+   [:div
     [:input#field-premium-account-acceptance
      (form.dom/checkbox {:on-change update-field}
-                        field)]
-    [errors field]]])
+                        field)]]
+   [field-status field]])
 
 (defn form []
   (let [form @hello-form]
-    [:div.max-w-md.space-y-4
+    [:div.max-w-5xl.space-y-4
      [:h1 "Welcome"]
      [:p.space-y-4
       [:code.block
@@ -130,11 +134,10 @@
        "When all errors (though not necessarily all warnings) have been
        resolved, the submit button will become enabled."]]
      [:form.space-y-4 {:on-submit (fn [e] (.preventDefault e))}
-      [:div.grid.grid-cols-2.gap-4
-       [basic-input {:id "field-full-name"} (form/field-by-path form [:full-name])]
-       [security-question-input
-        (form/field-by-path form [:security-question])
-        (form/field-by-path form [:security-response])]
+      [:div.grid.grid-cols-1.md:grid-cols-3.gap-1.md:gap-4
+       [basic-input {:id "field-full-name", :auto-focus true} (form/field-by-path form [:full-name])]
+       [security-question-input (form/field-by-path form [:security-question])]
+       [basic-input {:id "field-response"} (form/field-by-path form [:security-response])]
        [password-input {:id "field-password"} (form/field-by-path form [:password])]
        [password-input {:id "field-password-confirmation"} (form/field-by-path form [:password-confirmation])]
        [premium-account-input (form/field-by-path form [:premium-account])]]
